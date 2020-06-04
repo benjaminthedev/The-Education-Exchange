545a2d12d8dec2273b8236a020514b8e4819976d
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-06-04 12:00:27 +0100

    new templates added

---
+++ education-exchange-child-theme/boxes-example.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Template Name: Boxes Example
+ *
+ *
+ * @package OceanWP WordPress theme
+ */
+
+get_header(); ?>
+
+	<?php do_action( 'ocean_before_content_wrap' ); ?>
+
+	<div id="content-wrap" class="container clr">
+
+		<?php do_action( 'ocean_before_primary' ); ?>
+
+		<div id="primary" class="content-area clr">
+
+			<?php do_action( 'ocean_before_content' ); ?>
+
+			<div id="content" class="site-content clr">
+
+				<?php do_action( 'ocean_before_content_inner' ); ?>
+
+    
+                    <div class="custom-boxes">
+                    
+                        
+                    
+                    
+                    </div>
+
+
+
+				<?php do_action( 'ocean_after_content_inner' ); ?>
+
+			</div><!-- #content -->
+
+			<?php do_action( 'ocean_after_content' ); ?>
+
+		</><!-- #primary -->
+
+		<?php do_action( 'ocean_after_primary' ); ?>
+
+	</div><!-- #content-wrap -->
+
+	<?php do_action( 'ocean_after_content_wrap' ); ?>
+
+<?php get_footer(); ?>
