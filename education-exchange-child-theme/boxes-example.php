565abcbcb4ba1cb0a4f2f1353a7bf67e58deae7a
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-06-05 15:49:14 +0100

    Added in the ACF

--- education-exchange-child-theme/boxes-example.php
+++ education-exchange-child-theme/boxes-example.php
@@ -243,81 +243,93 @@ article.box_rule:hover .box-rule-details  {
                     <div class="custom-boxes">
 
 
-                        <a href="#" class="main-box one" >
+                        <a href="<?php the_field('box_1_link_url'); ?>" class="main-box one" >
                             <article class="box_rule">
                                 <div class="golden-rule__number">1</div>
                                   
 
                                 <div class="box-rule-details">
-                                    <p>This is the part which is show on hover</p>
+                                    <p><?php the_field('box_1_hover_text'); ?></p>
                                 </div>    
 
-                                  <h1 class="box-heading">Heading Now</h1>
+                                  <h1 class="box-heading">
+                                    <?php the_field('box_1_heading'); ?>
+                                  </h1>
                             </article>
                         </a>
 
-                        <a href="#" class="main-box two">
+                        <a href="<?php the_field('box_2_link_url'); ?>" class="main-box two">
                             <article class="box_rule">
                                 <div class="golden-rule__number">2</div>
                                     
 
                                 <div class="box-rule-details">
-                                    <p>This is the part which is show on hover</p>
+                                    <p><?php the_field('box_2_hover_text'); ?></p>
                                 </div>    
 
-                                <h1 class="box-heading">Heading Now</h1>
+                                   <h1 class="box-heading">
+                                    <?php the_field('box_2_heading'); ?>
+                                  </h1>
                             </article>
                         </a>
 
-                        <a href="#" class="main-box three">
+                        <a href="<?php the_field('box_3_link_url'); ?>" class="main-box three">
                             <article class="box_rule">
                                 <div class="golden-rule__number">3</div>
                                     
 
                                 <div class="box-rule-details">
-                                    <p>This is the part which is show on hover</p>
+                                    <p><?php the_field('box_3_hover_text'); ?></p>
                                 </div>    
 
-                                <h1 class="box-heading">Heading Now</h1>
+                                 <h1 class="box-heading">
+                                    <?php the_field('box_3__heading'); ?>
+                                  </h1>
                             </article>
                         </a>       
                         
-                        <a href="#" class="main-box four">
+                        <a href="<?php the_field('box_4_link_url'); ?>" class="main-box four">
                             <article class="box_rule">
                                 <div class="golden-rule__number">4</div>
                                     
 
                                 <div class="box-rule-details">
-                                    <p>This is the part which is show on hover</p>
+                                    <p><?php the_field('box_4_hover_text'); ?></p>
                                 </div>    
 
-                                <h1 class="box-heading">Heading Now</h1>
+                                 <h1 class="box-heading">
+                                    <?php the_field('box_4_heading'); ?>
+                                  </h1>
                             </article>
                         </a>      
                         
-                        <a href="#" class="main-box five">
+                        <a href="<?php the_field('box_5_link_url'); ?>" class="main-box five">
                             <article class="box_rule">
                                 <div class="golden-rule__number">5</div>
                                     
 
                                 <div class="box-rule-details">
-                                    <p>This is the part which is show on hover</p>
+                                    <p><?php the_field('box_5_hover_text'); ?></p>
                                 </div>    
 
-                                <h1 class="box-heading">Heading Now</h1>
+                                  <h1 class="box-heading">
+                                    <?php the_field('box_5_heading'); ?>
+                                  </h1>
                             </article>
                         </a>      
                         
-                        <a href="#" class="main-box six">
+                        <a href="<?php the_field('box_6_link_url'); ?>" class="main-box six">
                             <article class="box_rule">
                                 <!-- <div class="golden-rule__number">1</div> -->
                                     
 
                                 <div class="box-rule-details">
-                                    <p>This is the part which is show on hover</p>
+                                    <p><?php the_field('box_6_hover_text'); ?></p>
                                 </div>    
                                 
-                                <h1 class="box-heading">Heading Now</h1>
+                                 <h1 class="box-heading">
+                                    <?php the_field('box_6_heading'); ?>
+                                  </h1>
 
                                 <!-- <div class="arrow">></div> -->
                             </article>
