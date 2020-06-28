4d3b918431168a58248057ccc61405189d7a8d4b
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-06-28 13:39:02 +0100

    updated css and JS

--- education-exchange-child-theme/custom-js/app.js
+++ education-exchange-child-theme/custom-js/app.js
@@ -10,6 +10,82 @@ if (document.getElementsByTagName('body')[0].classList.contains("single")) {
 }
 
 
+//Time checker each second
+//Then if statement - select2-container--open
+
+//Then the changes!
+
+
+setInterval(function () {
+    console.log("timer running");
+
+    // if (document.querySelectorAll('.select2-container')[0].classList.contains("open")) {
+    //     console.log("contains open");
+    // } else {
+    //     console.log("contains closed");
+    // }
+
+
+
+}, 1000);
+
+
+
+
+
+
+
+
+// User Submitted Links:
+// function after3Seconds() {
+//     const getClassToListenTo = document.querySelector('.select2');
+//     console.log(getClassToListenTo);
+
+
+
+
+//     const checkCats = document.querySelectorAll('.select2-results__option');
+//     // checkCats[0].style.backgroundColor = "red";
+//     checkCats[0].classList.add('newBG');
+
+
+// }
+
+// setTimeout(after3Seconds, 5000);
+
+
+
+
+
+
+
+
+// document.addEventListener("click", function () {
+//     document.getElementById("demo").innerHTML = "Hello World";
+// });
+
+
+
+
+
+
+
+// checkCats[7].style.backgroundColor = "#0073aa";
+
+// checkCats[14].style.backgroundColor = "#0073aa";
+
+// checkCats[21].style.backgroundColor = "#0073aa";
+
+// checkCats[28].style.backgroundColor = "#0073aa";
+
+
+
+
+
+
+
+
+
 
 
 
