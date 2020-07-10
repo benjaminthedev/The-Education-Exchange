9fce05519cf061d1e28efd7b4f743a761cf3e7bb
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-07-10 07:48:05 +0100

    updated Forum JS

--- education-exchange-child-theme/custom-js/forums.js
+++ education-exchange-child-theme/custom-js/forums.js
@@ -1,4 +1,16 @@
-console.log('forums JS Loaded');
+console.log('Forums JS Loaded');
+
+const accountLinkClicked = document.querySelectorAll('.bbp-author-link');
+
+accountLinkClicked.forEach(function (link) {
+    link.addEventListener('click', event => {
+        link.href = "/my-account/";
+    });
+});
+
+// BBpress forums text changes
+const forumTitle = document.querySelectorAll('.li.bbp-forum-info');
+console.log(forumTitle);
 
 //BBpress text change
 if (document.getElementsByTagName('body')[0].classList.contains("bbpress")) {
@@ -6,23 +18,3 @@ if (document.getElementsByTagName('body')[0].classList.contains("bbpress")) {
     // bbPressWarnTextChange[1].innerText = "There are no posts here yet. Be the first to connect!";
     bbPressWarnTextChange[1].innerText = "Be the first to contribute!";
 }
-
-// BBpress forums text changes
-
-const forumTitle = document.querySelectorAll('.li.bbp-forum-info');
-console.log(forumTitle);
-
-console.log('Author CLicked before');
-
-const accountLinkClicked = document.querySelectorAll('.bbp-author-link');
-console.log(accountLinkClicked);
-
-
-
-
-
-document.querySelectorAll('.bbp-author-link').addEventListener('click', event => {
-    //handle click
-    alert('Author CLicked');
-    window.location.href = "http://www.w3schools.com";
-});
