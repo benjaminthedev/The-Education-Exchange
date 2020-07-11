622d98815713200136c4c568e49518f346076844
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-07-11 08:05:09 +0100

    Theme updates

--- education-exchange-child-theme/custom-js/app.js
+++ education-exchange-child-theme/custom-js/app.js
@@ -1,6 +1,11 @@
 //Main JS
 console.log('Welcome to The Education Exchange');
 
+
+
+
+
+
 // const regLinkBBP = document.querySelectorAll('.bbp-lostpass-link');
 // console.log(regLinkBBP);
 
@@ -13,12 +18,14 @@ console.log('Welcome to The Education Exchange');
 Checks to see if body class list contains single if so then
 const changes the text on the bubble
 */
-// if (document.getElementsByTagName('body')[0].classList.contains("single-post")) {
-//     const bubbleTextChange = document.getElementById('wpd-bubble-add-message').innerText = "Share your reflections";
+if (document.getElementsByTagName('body')[0].classList.contains("single-post")) {
+    const bubbleTextChange = document.getElementById('wpd-bubble-add-message').innerText = "What do you think? Share your reflections.";
+    console.log(bubbleTextChange);
 
-//     const doubleFacebook = document.querySelectorAll('.elementor-share-btn_facebook');
-//     doubleFacebook[1].style.display = "none";
-// }
+    //     const doubleFacebook = document.querySelectorAll('.elementor-share-btn_facebook');
+    //     doubleFacebook[1].style.display = "none";
+    // 
+}
 
 
 
