a947ae91f7769f8feb6479f5dd341065be03f8b1
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-07-20 10:53:06 +0100

    Minified the CSS and JS

--- education-exchange-child-theme/custom-js/home.js
+++ education-exchange-child-theme/custom-js/home.js
@@ -1,18 +1,1 @@
-//If logged in show this text
-const bodyCheck = document.getElementsByTagName('body')[0];
-
-if (bodyCheck.classList.contains("logged-in")) {
-    const areYouLoggedIn = document.querySelectorAll('.standardBtn-New  ');
-    areYouLoggedIn[0].innerText = "Visit Your Account";
-    console.log(areYouLoggedIn);
-    console.log('User Logged In');
-}
-
-
-if (!bodyCheck.classList.contains("logged-in")) {
-    const redirect = document.querySelectorAll('.standardBtn');
-    redirect[0].href = "/register/"
-    console.log("not logged in");
-
-}
-
+const bodyCheck = document.getElementsByTagName("body")[0]; if (bodyCheck.classList.contains("logged-in")) { const e = document.querySelectorAll(".standardBtn-New  "); e[0].innerText = "Visit Your Account", console.log(e), console.log("User Logged In") } if (!bodyCheck.classList.contains("logged-in")) { document.querySelectorAll(".standardBtn")[0].href = "/register/", console.log("not logged in") }
