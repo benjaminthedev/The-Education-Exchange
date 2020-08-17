b958951b6ff198d69b937edbbe20c44e0c14b386
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-08-17 11:15:44 +0100

    Theme Updated with JS

---
+++ education-exchange-child-theme/custom-js/buildingLearningLinkJS.js
@@ -0,0 +1,7 @@
+//Building learning independence
+
+console.log('Building learning independence');
+
+const isCheckedTheme = document.querySelectorAll('input[type="checkbox"]');
+
+isCheckedTheme[0].checked = true;
